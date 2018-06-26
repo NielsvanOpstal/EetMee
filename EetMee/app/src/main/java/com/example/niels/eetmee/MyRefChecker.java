bdd767a98a96ecdc417afd55d6e44fb8ac17f3ce
niels <niels_v_opstal@live.nl>
2018-06-26 15:21:09 +0200

    myrefChecker refactored

---
+++ EetMee/app/src/main/java/com/example/niels/eetmee/MyRefChecker.java
@@ -0,0 +1,27 @@
+/*
+EetMee
+Niels van Opstal 11021519
+
+This class lets you check whether there is still a reference to the Firebase database and authentication
+ */
+package com.example.niels.eetmee;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static com.example.niels.eetmee.MainActivity.MYREF;
+import static com.example.niels.eetmee.MainActivity.mAuth;
+
+public class MyRefChecker {
+
+//    Checks the Firebase database and authentication references, if null: get the again
+    public void checker() {
+        if (MYREF == null) {
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            MYREF = database.getReference();
+        }
+        if (mAuth.getUid() == null) {
+            mAuth = FirebaseAuth.getInstance();
+        }
+    }
+}
